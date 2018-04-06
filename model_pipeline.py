8c2abec3f6cceb2b404549a73eab8abff3535519
Karthik Rajasethupathy <krajasethupathy@gilt.com>
2018-04-06 18:25:17 -0400

    add ingredients for dataset and preprocessing, with a simple variant

--- model_pipeline.py
+++ model_pipeline.py
@@ -1,28 +1,53 @@
-from numpy.random import permutation
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_val_score
-from sacred import Experiment
+from sklearn.model_selection import cross_val_score,train_test_split
+from sacred import Experiment,Ingredient
 import pandas as pd
+import numpy as np
 
-ex = Experiment('titantic')
+
+train_data_ingredient = Ingredient('train_dataset')
+
+@train_data_ingredient.config
+def load_data_cfg():
+    filename = 'data/train.csv'
+    target = 'Survived'
+    split_size = .75
+
+@train_data_ingredient.capture
+def load_data(filename,target,split_size):
+    data = pd.read_csv(filename)
+    features = [i for i in data.columns if i!= target]
+    return train_test_split(data[features],data[target],train_size=split_size)
+
+preprocess_ingredient = Ingredient('preprocess')
+
+@preprocess_ingredient.config
+def cfg_preprocess():
+    features = ['Fare','SibSp','Parch']
+
+@preprocess_ingredient.named_config
+def variant_preprocess_data():
+    features = ['Fare', 'SibSp']
+
+@preprocess_ingredient.capture
+def preprocess_data(df, features):
+    return df[features]
+
+ex = Experiment('titantic', ingredients=[train_data_ingredient, preprocess_ingredient])
 
 @ex.config
 def cfg():
-  penalty = 'l2'
-  fit_intercept = False
-  features = ['Fare','SibSp','Parch']
-  folds = 10
+    penalty = 'l2'
+    fit_intercept = False
+    #folds = 10
 
 @ex.automain
-def run(penalty, fit_intercept,features,folds):
+def run(penalty, fit_intercept):
 
-	train = pd.read_csv('data/train.csv')
+    X_train, X_val, Y_train, Y_val = load_data()
 
-	X_train = train[features]
-	Y_train = train[['Survived']]
+    clf_lg = LogisticRegression(penalty=penalty,fit_intercept=fit_intercept)
+    clf_lg.fit(preprocess_data(X_train),Y_train)
 
-	clf_lg = LogisticRegression(penalty=penalty,fit_intercept=fit_intercept)
-	clf_lg.fit(X_train,Y_train)
-
-	return cross_val_score(clf_lg,X_train,Y_train,cv=folds).mean()
+    return clf_lg.score(preprocess_data(X_val),Y_val)
 
