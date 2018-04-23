0fdced41f13899c5350597b7189d2cd087e0ed27
Jason Tam <JasonTam22@gmail.com>
2018-04-23 13:15:04 -0400

    better tracking (#7)
    * better tracking
    
    * up reaqdme

--- experiments/track_metrics.py
+++ experiments/track_metrics.py
@@ -1,8 +1,7 @@
-from sklearn.linear_model import SGDClassifier
+from sklearn.neural_network import MLPClassifier
 from sacred import Experiment
 from ingredients.data import train_data_ingredient, load_data
 from ingredients.preproc import preprocess_ingredient, preprocess_data
-import numpy as np
 
 ex = Experiment('titanic',
                 ingredients=[train_data_ingredient, preprocess_ingredient])
@@ -10,19 +9,26 @@ ex = Experiment('titanic',
 
 @ex.config
 def cfg():
-    penalty = 'l1'
-    learning_rate = 'optimal'
-    n_epochs = 10
+    n_epochs = 500
 
 
 @ex.automain
-def run(penalty, learning_rate, n_epochs, _run):
+def run(n_epochs, _run):
     X_train, X_val, Y_train, Y_val = load_data()
+    clf = MLPClassifier(warm_start=True, max_iter=1)
 
-    clf_lg = SGDClassifier(penalty=penalty, learning_rate=learning_rate, max_iter=1, shuffle=True)
+    X_train_pp = preprocess_data(X_train)
+    X_val_pp = preprocess_data(X_val)
 
+    val_acc_l = []
     for i in range(n_epochs):
 
-        clf_lg.partial_fit(preprocess_data(X_train), Y_train, classes=np.unique(Y_train))
-        _run.log_scalar("training.accuracy", clf_lg.score(preprocess_data(X_train), Y_train), i)
-        _run.log_scalar("validation.accuracy", clf_lg.score(preprocess_data(X_val), Y_val), i)
+        clf.fit(X_train_pp, Y_train)
+        train_acc = clf.score(X_train_pp, Y_train)
+        val_acc = clf.score(X_val_pp, Y_val)
+        val_acc_l.append(val_acc)
+        ex.log_scalar("training.accuracy", train_acc, i)
+        ex.log_scalar("validation.accuracy", val_acc, i)
+
+    return max(val_acc_l)
+
